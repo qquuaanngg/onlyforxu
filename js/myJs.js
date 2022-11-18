145adf6bf5101e8b54f812294e8a64614cf8ee76
qquuaanngg <118493249+qquuaanngg@users.noreply.github.com>
2022-11-18 10:44:35 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,6 +1,6 @@
 const textConfig = {
   text1: "Hé luu Xù!",
-  text2: "Tôi có điều này muốn hỏi Xù",
+  text2: "Tôi có điều này muốn hỏi Xù được k ???",
   text3: "Đầu tiên mong Xù tha lỗi vì hẹn Xù đi chơi nhưng sủi tận 1 tháng 3 ngày ",
   text4: "Và mong Xù có thể lại dành chút thời gian, cho kẻ thất hứa này hẹn gặp được k ???",
   text5: "Dỗi, k cho :333",
